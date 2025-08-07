c58c6cc4210044422a5e11847d1926fce8f4b615
IMANI <kiplagatfaith88@gmail.com>
2025-08-07 13:24:49 +0300

    Few Adjustment

--- built-inDs.py
+++ built-inDs.py
@@ -1,24 +1,19 @@
 my_list=[ ]
-print(my_list)
 
 my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
-print(my_list)
 
 my_list.insert(1,15)
-print(my_list)
 
 my_list.extend([50 ,60 ,70])
-print(my_list)
 
 my_list.pop(-1)
-print(my_list)
  
 my_list.sort()
-print(my_list)
 
 Thiry= my_list.index(30)
 print(Thiry)
+print(my_list)
 
